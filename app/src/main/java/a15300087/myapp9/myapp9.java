be197ead487fd7cdc3a3703e02717aa01ac43b9a
isai <isaig2015@gmail.com>
2018-05-03 13:52:00 -0500

    l

---
+++ app/src/main/java/a15300087/myapp9/myapp9.java
@@ -0,0 +1,13 @@
+package a15300087.myapp9;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class myapp9 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.myapp9);
+    }
+}
